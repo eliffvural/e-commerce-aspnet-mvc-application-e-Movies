64aead13b8a046ccce9084eb28dcaad93e2e639e
Elif Vural <elifvural019@gmail.com>
2024-12-24 14:10:31 +0300

    actor features added

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -1,7 +1,15 @@
-﻿namespace eTickets.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
 {
     public class Actor
     {
-        public String ProfilePictureURL {  get; set; }
+        [Key]
+        public int Id { get; set; }
+        public String ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+
     }
 }
