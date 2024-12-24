a3c1c8e18c65694fdc872fcc5158f3cdfe9bcff4
Elif Vural <elifvural019@gmail.com>
2024-12-24 18:46:49 +0300

    entity entegrated

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -16,6 +16,10 @@ namespace eTickets.Data
                 am.ActorId,
                 am.MovieId
             });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actor_Movies).HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actor_Movies).HasForeignKey(m => m.ActorId);
+
             base.OnModelCreating(modelBuilder);
         }
     }
