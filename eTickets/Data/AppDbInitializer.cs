729627b07a4f9ec8faa0c8884191ea7c52d4e08c
Elif Vural <elifvural019@gmail.com>
2024-12-25 00:50:59 +0300

    AppDbInitializer creating

---
+++ eTickets/Data/AppDbInitializer.cs
@@ -0,0 +1,23 @@
+﻿namespace eTickets.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope()) 
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+                context.Database.EnsureCreated();
+
+                //Cinema
+                //Actors
+                //Producers
+                //Movies
+                //Actors-Movies
+                
+            
+            }
+        }
+    }
+}
