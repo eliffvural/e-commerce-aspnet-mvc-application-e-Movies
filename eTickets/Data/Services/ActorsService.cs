506603f1573e25e646a707006a74285f67cb493e
Elif Vural <elifvural019@gmail.com>
2024-12-26 01:54:47 +0300

    IActorsMovie coding

--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -1,6 +1,42 @@
-﻿namespace eTickets.Data.Services
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Services
 {
-    public class ActorsService: IActorsService
+    public class ActorsService : IActorsService
     {
+
+        private readonly AppDbContext _context;
+
+        public ActorsService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        void IActorsService.Add(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IActorsService.Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Actor>>GetAll()
+        {
+            var result =await  _context.Actors.ToListAsync();
+            return result;
+        }
+
+        Actor IActorsService.GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Actor IActorsService.Update(int id, Actor newActor)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
