6afbd416b6b076701ae67988625dd1aa29778f64
Elif Vural <elifvural019@gmail.com>
2024-12-26 01:35:45 +0300

    ActorsService class created

--- eTickets/Data/Services/IActorsService.cs
+++ eTickets/Data/Services/IActorsService.cs
@@ -5,5 +5,9 @@ namespace eTickets.Data.Services
     public interface IActorsService
     {
         IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int  id, Actor newActor);
+        void Delete(int id);
     }
 }
