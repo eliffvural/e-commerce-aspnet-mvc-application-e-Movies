f84fcd58fb8a730679a81fd1c0b92ebb364db62d
Elif Vural <elifvural019@gmail.com>
2024-12-27 00:31:28 +0300

    EntityBaseRepository ve ActorsService güncellemeleri
    EntityBaseRepository.cs dosyasına gerekli ad alanları eklendi.
    AppDbContext bağımlılığı eklendi ve yapıcı metodda başlatıldı.
    GetAllAsync ve GetByIdAsync metodları veritabanı işlemleri yapacak şekilde güncellendi.
    ActorsService.cs dosyasından GetAllAsync ve GetByIdAsync metodları kaldırıldı.
    UpdateAsync metodu ActorsService.cs dosyasına eklendi ve aktör güncelleme işlemi yapacak şekilde tanımlandı.

--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -1,8 +1,18 @@
 ﻿
+using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace eTickets.Data.Base
 {
     public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
+
+        private readonly AppDbContext _context;
+
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
         public Task AddAsync(T entity)
         {
             throw new NotImplementedException();
@@ -13,15 +23,25 @@ namespace eTickets.Data.Base
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<T>> GetAllAsync()
+        public async Task<IEnumerable<T>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var result = await _context.Set<T>().ToListAsync();
+            return result;
         }
 
-        public Task<T> GetByIdAsync(int id)
+        public async Task<T> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            return result;
         }
+
+
+
+
+
+
+
+
 
         public Task<T> UpdateAsync(int id, T entity)
         {
