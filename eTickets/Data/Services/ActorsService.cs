e1144aedd6ed9ab1d47cd326f7423f073962e907
Elif Vural <elifvural019@gmail.com>
2024-12-27 00:02:22 +0300

    Aktör düzenleme ve silme işlemleri eklendi
    `ActorsController.cs` dosyasına yeni `Edit` ve `Delete` işlemleri eklendi. Bu işlemler, aktörlerin düzenlenmesi ve silinmesi için gerekli olan GET ve POST metodlarını içerir.
    
    `ActorsService.cs` dosyasına `DeleteAsync` ve `UpdateAsync` metodları eklendi. Bu metodlar, aktörlerin veritabanından silinmesi ve güncellenmesi işlemlerini asenkron olarak gerçekleştirir.
    
    `IActorsService.cs` arayüzüne `DeleteAsync` ve `UpdateAsync` metodları eklendi. Bu, `ActorsService` sınıfının bu metodları uygulamasını gerektirir.
    
    `Create.cshtml` dosyasındaki bazı JavaScript kodları kaldırıldı.
    
    `Details.cshtml` dosyasındaki bir HTML hata düzeltildi (`asp-rout-id` `asp-route-id` olarak değiştirildi).
    
    `Delete.cshtml` dosyası eklendi. Bu dosya, bir aktörün silinmesi için bir onay formu içerir.
    
    `Edit.cshtml` dosyası eklendi. Bu dosya, bir aktörün bilgilerini güncellemek için bir form içerir.

--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -19,9 +19,11 @@ namespace eTickets.Data.Services
             await   _context.SaveChangesAsync();
         }
 
-        void IActorsService.Delete(int id)
+        public async Task DeleteAsync(int id)        
         {
-            throw new NotImplementedException();
+            var result = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
+            _context.Actors.Remove(result);
+            await _context.SaveChangesAsync();
         }
 
         public async Task<IEnumerable<Actor>>GetAllAsync()
@@ -36,9 +38,12 @@ namespace eTickets.Data.Services
             return result;
         }
 
-        Actor IActorsService.Update(int id, Actor newActor)
+        public async Task<Actor> UpdateAsync(int id, Actor newActor)
         {
-            throw new NotImplementedException();
+            _context.Update(newActor);
+            await _context.SaveChangesAsync();
+            return newActor;
+
         }
 
        
