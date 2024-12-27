cda7bde131e3cfb28fe65575581dabc71fcc38d4
Elif Vural <elifvural019@gmail.com>
2024-12-28 02:02:13 +0300

    cinemas service added

--- eTickets/Data/Services/ICinemasService.cs
+++ eTickets/Data/Services/ICinemasService.cs
@@ -1,6 +1,9 @@
-﻿namespace eTickets.Data.Services
+﻿using eTickets.Data.Base;
+using eTickets.Models;
+
+namespace eTickets.Data.Services
 {
-    public interface ICinemasService
+    public interface ICinemasService : IEntityBaseRepository<Cinema>
     {
     }
 }
