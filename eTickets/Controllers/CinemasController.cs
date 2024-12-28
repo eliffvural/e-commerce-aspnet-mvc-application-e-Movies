f1491ec20042a0817f73e4f05075046d073104b1
Elif Vural <elifvural019@gmail.com>
2024-12-28 10:24:34 +0300

    CinemasController'a Create metodu eklendi
    CinemasController.cs dosyasına eTickets.Models için using ifadesi eklendi.
    CinemasController sınıfına Create adında yeni bir GET ve POST metodu eklendi.
    Index.cshtml dosyasında, "Add New" düğmesinin asp-controller özelliği kaldırıldı.

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,5 +19,23 @@ namespace eTickets.Controllers
             var allCinemas = await _service.GetAllAsync();
             return View(allCinemas);
         }
+
+
+
+
+
+        //GET: Cinemas/Create
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Logo,Name,Description")]Cinema)
+        {
+
+        }
+      
     }
 }
