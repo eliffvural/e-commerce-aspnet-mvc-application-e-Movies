05ac23045b57cdfa47fddecd9dfdc02e151391bc
Elif Vural <elifvural019@gmail.com>
2024-12-28 10:43:11 +0300

    details method added to cinemas controller

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -54,6 +54,18 @@ namespace eTickets.Controllers
             return RedirectToAction(nameof(Index));
 
         }
-      
+
+
+
+
+        //GET: Cinemas/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+
     }
 }
