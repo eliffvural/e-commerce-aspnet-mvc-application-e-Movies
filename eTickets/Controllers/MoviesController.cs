b8a0524beb1bd0cd4b6c9a884f30c46e4bbdb336
Elif Vural <elifvural019@gmail.com>
2024-12-29 15:37:52 +0300

    Edit method added  to MoviesController NewMovieVM updated

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -63,5 +63,43 @@ namespace eTickets.Controllers
             await _service.AddNewMovieAsync(movie);
             return RedirectToAction(nameof(Index));
         }
+
+
+
+
+
+        //GET:  Movies/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+
+            var movieDetails = await _service.GetMovieByIdAsync(id);
+
+            if (movieDetails == null) return View("NotFound");
+
+            var response= new NewMovieVM()
+            {
+                Id = movieDetails.Id,
+                Name = movieDetails.Name,
+                Description = movieDetails.Description,
+                Price = movieDetails.Price,
+                ImageURL = movieDetails.ImageURL,
+                CinemaId = movieDetails.CinemaId,
+                StartDate = movieDetails.StartDate,
+                EndDate = movieDetails.EndDate,
+                MovieCategory = movieDetails.MovieCategory,
+                ProducerId = movieDetails.ProducerId,
+                ActorIds = movieDetails.Actor_Movies.Select(n => n.ActorId).ToList()
+            };
+
+
+            var movieDropdownsData = await _service.GetNewMovieDropdownsVM();
+
+
+            ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "Name");
+            ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+            ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+
+            return View();
+        }
     }
 }
