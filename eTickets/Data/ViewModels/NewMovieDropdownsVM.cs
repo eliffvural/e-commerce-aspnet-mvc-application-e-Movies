1eb15fba43a6e84ce389b94365ab7660b230c859
Elif Vural <elifvural019@gmail.com>
2024-12-29 13:45:21 +0300

    new list features added to NewMovieDropdownsVM

--- eTickets/Data/ViewModels/NewMovieDropdownsVM.cs
+++ eTickets/Data/ViewModels/NewMovieDropdownsVM.cs
@@ -1,6 +1,11 @@
-﻿namespace eTickets.Data.ViewModels
+﻿using eTickets.Models;
+
+namespace eTickets.Data.ViewModels
 {
     public class NewMovieDropdownsVM
     {
+        public List<Producer> Producers { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<Actor> Actors { get; set; }
     }
 }
