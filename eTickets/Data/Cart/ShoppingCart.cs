af106516aa761f4db32f0ffa38c1939d56fd78bd
Elif Vural <elifvural019@gmail.com>
2025-01-02 00:03:51 +0300

    shopping cart cs updated

--- eTickets/Data/Cart/ShoppingCart.cs
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -7,11 +7,25 @@ namespace eTickets.Data.Cart
         public AppDbContext _context { get; set; }
         public string ShoppingCartId { get; set; }
         public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+        
+        
+        
         public  ShoppingCart(AppDbContext context)
         {
             _context = context;
         }
 
+      public static ShoppingCart GetShoppingCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            var context = services.GetService<AppDbContext>();
+
+            string cartId= session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+        
+        
         public void AddItemToCart(Movie movie)
         {
             var shoppingCartItem= _context.
