463ab71871e320112d2a83758bba52eab6ee749a
Elif Vural <elifvural019@gmail.com>
2025-01-02 15:49:21 +0300

    IOrdersService and OrdersService classes added

---
+++ eTickets/Data/Services/OrdersService.cs
@@ -0,0 +1,26 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Services
+{
+    public class OrdersService : IOrdersService
+    {
+
+        private readonly AppDbContext _context;
+
+        public OrdersService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Order>> GetOrdersByUserIdAsync(string userId)
+        {
+            var orders = await _context.Orders.Include(n => n.OrderItems).ThenInclude(n => n.Movie).Where(n => n.UserId == userId).ToListAsync();
+            return orders;
+        }
+
+        public Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
