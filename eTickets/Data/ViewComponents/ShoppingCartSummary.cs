908e16f35f96cace654de8411cdf6d825c047b9d
Elif Vural <elifvural019@gmail.com>
2025-01-02 15:22:44 +0300

    Added constructor and Invoke method to ShoppingCartSummary class

--- eTickets/Data/ViewComponents/ShoppingCartSummary.cs
+++ eTickets/Data/ViewComponents/ShoppingCartSummary.cs
@@ -9,6 +9,19 @@ namespace eTickets.Data.ViewComponents
     {
         private readonly ShoppingCart _shoppingCart;
 
-       
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+
+        }
+
     }
 }
