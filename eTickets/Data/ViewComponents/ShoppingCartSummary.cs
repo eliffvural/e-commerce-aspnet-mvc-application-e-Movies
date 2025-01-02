70397de610d904ef19e6543f10363553f84ba2fd
Elif Vural <elifvural019@gmail.com>
2025-01-02 15:20:39 +0300

    ShoppingCartSummary class updates

--- eTickets/Data/ViewComponents/ShoppingCartSummary.cs
+++ eTickets/Data/ViewComponents/ShoppingCartSummary.cs
@@ -1,6 +1,14 @@
-﻿namespace eTickets.Data.ViewComponents
+﻿using eTickets.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Data.ViewComponents
 {
-    public class ShoppingCartSummary
+
+
+    public class ShoppingCartSummary: ViewComponent
     {
+        private readonly ShoppingCart _shoppingCart;
+
+       
     }
 }
