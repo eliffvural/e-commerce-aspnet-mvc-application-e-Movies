831751f82f4e77598a3fe6e0684e17bba08ca88f
Elif Vural <elifvural019@gmail.com>
2025-01-03 13:31:25 +0300

    register vm, register button created and updated

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -23,6 +23,39 @@ namespace eTickets.Controllers
 
 
         public IActionResult Login() => View(new LoginVM());
-        
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if(!ModelState.IsValid)  return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+
+            if(user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if(passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Movies");
+                    }
+                }
+
+                TempData["Error"] = "wrong credentials. Please, try again!";
+                return View(loginVM);
+            }
+
+            TempData["Error"] = "wrong credentials. Please, try again!";
+            return View(loginVM);
+
+        }
+
+
+
+
+        public IActionResult Register() => View(new RegisterVM());
     }
 }
