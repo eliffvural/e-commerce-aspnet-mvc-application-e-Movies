839d23925060eb88feb49d6f5ae08fb2595bc3a6
Elif Vural <elifvural019@gmail.com>
2025-01-03 12:17:08 +0300

    AccountController updates and ve dependence adds

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eTickets.Data;
+using eTickets.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eTickets.Controllers
 {
     public class AccountController : Controller
     {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+            private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+
+
+        public AccountController() 
+        {
+        }
+
+
         public IActionResult Index()
         {
             return View();
